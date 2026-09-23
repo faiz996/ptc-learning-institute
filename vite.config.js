dd32f44561819ac75301a276f5d68ea894e8dc16
faiz996 <alifaizp10@gmail.com>
2026-09-23 14:45:33 +0530

    feat: initial project setup

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,25 @@
 // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     allowedHosts: true,
+//   },
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/ptc-learning-institute/",
+
   server: {
     allowedHosts: true,
   },
-})
+});
